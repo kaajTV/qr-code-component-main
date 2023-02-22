22a8237e1021abe693697cb50a66c10fbf268df9
Anton kaaj Holmgren <anton.holmgren3@hotmail.com>
2023-02-22 09:03:00 +0100

    Commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,10 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "D:\\FrontEndMentor Projects\\qr-code-component-main\\src\\app\\page": [
+      "D:\\FrontEndMentor Projects\\qr-code-component-main\\src\\app\\globals.css"
+    ]
+  },
+  "D:\\FrontEndMentor Projects\\qr-code-component-main\\src\\app\\layout.js": [
+    "D:\\FrontEndMentor Projects\\qr-code-component-main\\src\\app\\globals.css"
+  ]
+}
---
+++ .next/static/webpack/app/page.ee39290c3b4aaa1f.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=D%3A%5CFrontEndMentor%20Projects%5Cqr-code-component-main%5Cnode_modules%5Cnext%5Cdist%5Cclient%5Cimage.js&modules=D%3A%5CFrontEndMentor%20Projects%5Cqr-code-component-main%5Cimages%5Cimage-qr-code.png&server=false!":
+/*!*************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=D%3A%5CFrontEndMentor%20Projects%5Cqr-code-component-main%5Cnode_modules%5Cnext%5Cdist%5Cclient%5Cimage.js&modules=D%3A%5CFrontEndMentor%20Projects%5Cqr-code-component-main%5Cimages%5Cimage-qr-code.png&server=false! ***!
+  \*************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.t.bind(__webpack_require__, /*! ./node_modules/next/dist/client/image.js */ \"(app-client)/./node_modules/next/dist/client/image.js\", 23));\nPromise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./images/image-qr-code.png */ \"(app-client)/./images/image-qr-code.png\"))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9idWlsZC93ZWJwYWNrL2xvYWRlcnMvbmV4dC1mbGlnaHQtY2xpZW50LWVudHJ5LWxvYWRlci5qcz9tb2R1bGVzPUQlM0ElNUNGcm9udEVuZE1lbnRvciUyMFByb2plY3RzJTVDcXItY29kZS1jb21wb25lbnQtbWFpbiU1Q25vZGVfbW9kdWxlcyU1Q25leHQlNUNkaXN0JTVDY2xpZW50JTVDaW1hZ2UuanMmbW9kdWxlcz1EJTNBJTVDRnJvbnRFbmRNZW50b3IlMjBQcm9qZWN0cyU1Q3FyLWNvZGUtY29tcG9uZW50LW1haW4lNUNpbWFnZXMlNUNpbWFnZS1xci1jb2RlLnBuZyZzZXJ2ZXI9ZmFsc2UhLmpzIiwibWFwcGluZ3MiOiJBQUFBLHlNQUFvSTtBQUNwSSIsInNvdXJjZXMiOlsid2VicGFjazovL19OX0UvPzYwM2QiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImVhZ2VyXCIgKi8gXCJEOlxcXFxGcm9udEVuZE1lbnRvciBQcm9qZWN0c1xcXFxxci1jb2RlLWNvbXBvbmVudC1tYWluXFxcXG5vZGVfbW9kdWxlc1xcXFxuZXh0XFxcXGRpc3RcXFxcY2xpZW50XFxcXGltYWdlLmpzXCIpO1xuaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImVhZ2VyXCIgKi8gXCJEOlxcXFxGcm9udEVuZE1lbnRvciBQcm9qZWN0c1xcXFxxci1jb2RlLWNvbXBvbmVudC1tYWluXFxcXGltYWdlc1xcXFxpbWFnZS1xci1jb2RlLnBuZ1wiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=D%3A%5CFrontEndMentor%20Projects%5Cqr-code-component-main%5Cnode_modules%5Cnext%5Cdist%5Cclient%5Cimage.js&modules=D%3A%5CFrontEndMentor%20Projects%5Cqr-code-component-main%5Cimages%5Cimage-qr-code.png&server=false!\n"));
+
+/***/ })
+
+});
